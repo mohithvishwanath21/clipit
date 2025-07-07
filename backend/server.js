cbce3066a7fdc0e5c0a65457bf9b924a276edad6
Mohith Vishwanath <mohithmpc21@gmail.com>
2025-07-07 11:28:58 +0530

    Add avatar images

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ import userRouter from "./routes/user.js";
 import { secureRoute } from "./middlewares/auth.js";
 import redirectUrlRouter from "./routes/redirecting.js";
 import contactRouter from "./routes/contact.js";
-
+app.use("/avatars", express.static("avatars"));
 // Connect to DB
 connectDB();
 
